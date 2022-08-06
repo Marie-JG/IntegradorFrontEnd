ae8f03503c128b37446a76c5498e2b326c0d8236
Marie-JG <99450393+Marie-JG@users.noreply.github.com>
2022-08-05 23:17:10 -0300

    Update logica.js

--- Trabajo práctico integrador JS/logica.js
+++ Trabajo práctico integrador JS/logica.js
@@ -1,60 +1,76 @@
+let precioEntrada = document.getElementById("precioEntrada").innerHTML;
+
+console.log(precioEntrada);
+
+let descuento1 = document.getElementById("descuentoEstudiante").innerHTML;
+let descuento2 = document.getElementById("descuentoTrainee").innerHTML;
+let descuento3 = document.getElementById("descuentoJunior").innerHTML;
+
+console.log(descuento1);
+console.log(descuento2);
+console.log(descuento3);
+
+let categoria = document.getElementById("categoria").innerHTML;
+
+let cant = document.querySelector("#cantidad");
+
 let total = document.getElementById("total").innerHTML;
 
 let resumen = document.getElementById("calcularPrecio");
 
 let borrado = document.getElementById("borrado");
 
-let textoOriginal;
+precioEntrada = parseFloat(precioEntrada);
+descuento1 = parseFloat(descuento1);
+descuento1 = parseFloat(descuento2);
+descuento1 = parseFloat(descuento3);
+cant = parseInt(cant);
 
-function mouseOver(event) {
+/*resumen.onmouseover = mouseOver;
+resumen.onmouseout = mouseOut;
+borrado.onmouseover = mouseOver;
+borrado.onmouseout = mouseOut;
+resumen.onclick = calcularPrecio;
+borrado.onclick = borrar;*/
+
+/*function mouseOver(event) {
   let textoOriginal = event.target.innerHTML;
   event.target.innerHTML = "Hacé click";
 }
 function mouseOut(event) {
   event.target.innerHTML = textoOriginal;
-}
+}*/
 
+// Función para calcular precio
 function calcularPrecio() {
+
   let precioFinal = 0;
-  let categoria = document.getElementById("categoria").innerHTML;
-  let cant = document.querySelector("#cantidad");
-  let precioEntrada = document.getElementById("precioEntrada").innerHTML;
 
-  console.log(precioEntrada);
-
-  let descuento1 = document.getElementById("descuentoEstudiante").innerHTML;
-  let descuento2 = document.getElementById("descuentoTrainee").innerHTML;
-  let descuento3 = document.getElementById("descuentoJunior").innerHTML;
-
-  console.log(descuento1);
-  console.log(descuento2);
-  console.log(descuento3);
-
-  if (categoria.innerHTML == "1") {
-    precioFinal = parseInt(precioEntrada - (precioEntrada * descuento1) / 100);
-  } else if (categoria.innerHTML == "2") {
-    precioFinal = parseInt(precioEntrada - (precioEntrada * descuento2) / 100);
+  if (categoria.innerHTML == "2") {
+    precioFinal = precioEntrada - (precioEntrada * descuento1) / 100;
   } else if (categoria.innerHTML == "3") {
-    precioFinal = parseInt(precioEntrada - (precioEntrada * descuento3) / 100);
+    precioFinal = precioEntrada - (precioEntrada * descuento2) / 100;
+  } else if (categoria.innerHTML == "4") {
+    precioFinal = precioEntrada - (precioEntrada * descuento3) / 100;
+  } else {
+    precioFinal = precioEntrada;
   }
 
-  total = total.innerHTML + (" " + precioFinal * cant);
-  console.log(total);
+  precioFinal = precioFinal * cant;
+
+  console.log(precioFinal);
+
+  total=document.getElementById("total").innerHTML = "$" + precioFinal;
 }
 
+// Función para borrar
 function borrar() {
+  
   document.getElementById("total").innerHTML = total.innerHTML;
-  document.getElementById("cantidad").innerHTML = "";
-  document.getElementById("categoria").innerHTML = "";
+  document.getElementById("cantidad").innerHTML = " ";
+  document.getElementById("categoria").innerHTML = " ";
 }
 
-resumen.onmouseover = mouseOver;
-resumen.onmouseout = mouseOut;
-borrado.onmouseover = mouseOver;
-borrado.onmouseout = mouseOut;
-resumen.onclick = calcularPrecio;
-borrado.onclick = borrar;
+document.getElementById("borrado").onclick = borrar;
 
-
-
- 
+document.getElementById("calcularPrecio").onclick = calcularPrecio;
